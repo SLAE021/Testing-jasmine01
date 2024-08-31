fcabfd612f73585cdd8a354de4f914e24c26a579
Samuel Angulo <samuelangulo70@gmail.com>
2024-08-31 13:53:00 +0200

    Solved lab

--- src/add.js
+++ src/add.js
@@ -1,7 +1,8 @@
-function add (numOne, numTwo) {
+it("should return undefined if any of the arguments is not a number"function add (numOne, numTwo) {
     if (numOne == undefined || numTwo == undefined) {
         return undefined;
     }
 
     return numOne + numTwo;
-}
+})
+
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -12,3 +12,28 @@ describe("Iteration 2", () => {
 })
 
 */
+describe("divide", function() {
+  
+    it("should be defined", function() {
+      expect(divide).toBeDefined();
+    });
+  
+    it("should return the division of two numbers", function() {
+      expect(divide(6, 2)).toBe(3);
+      expect(divide(10, 5)).toBe(2);
+    });
+  
+    it("should return undefined if any of the arguments is not provided", function() {
+      expect(divide(6)).toBeUndefined();
+      expect(divide()).toBeUndefined();
+    });
+  
+    it("should return Infinity if dividing by 0", function() {
+      expect(divide(6, 0)).toBe(Infinity);
+    });
+  
+    it("should return 0 if 0 is divided by any number", function() {
+      expect(divide(0, 5)).toBe(0);
+    });
+  
+  });
